adc2d2e4882ad0d3559e8ec9a0165069782e9218
Jacky Tian <xjtian94@gmail.com>
2014-02-16 22:25:10 -0500

    Add models for transactions app.

--- terpbooks/terpbooks/settings/base.py
+++ terpbooks/terpbooks/settings/base.py
@@ -1,12 +1,3 @@
-"""
-Django settings for terpbooks project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
 from unipath import Path
 
 # Paths
---
+++ terpbooks/transactions/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.conf import settings
+
+from books.models import Textbook
+
+
+class Listing(models.Model):
+    """
+    Listing for a textbook.
+    """
+    AVAILABLE = 'AV'
+    PENDING = 'PN'
+    SOLD = 'SD'
+
+    STATUS_CHOICES = (
+        (AVAILABLE, 'Available'),
+        (PENDING, 'Transaction Pending'),
+        (SOLD, 'Sold'),
+    )
+
+    date_created = models.DateField(auto_now_add=True, editable=False)
+
+    status = models.CharField(max_length=2,
+                              choices=STATUS_CHOICES,
+                              default=AVAILABLE)
+    asking_price = models.DecimalField(max_digits=10, decimal_places=2)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='listings')
+
+    book = models.OneToOneField(Textbook, related_name='listing')
+    comments = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return u'%s: %s' % (self.book.title, self.get_status_display())
+
+
+class TransactionRequestThread(models.Model):
+    """
+    Container for replies to/about a transaction request.
+    """
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_requests')
+    date_created = models.DateTimeField(auto_now_add=True, editable=False)
+
+    listing = models.ForeignKey(Listing, related_name='requests')
+
+    def __unicode__(self):
+        return u'%s' % self.listing.book.title
+
+
+class TransactionRequest(models.Model):
+    """
+    Transaction request for a textbook.
+    """
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    text = models.TextField(blank=True)
+
+    thread = models.ForeignKey(TransactionRequestThread, related_name='messages')
