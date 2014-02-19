3b1a4f0cd5939053d0380d890855b2d5bdbb8d14
Jacky Tian <xjtian94@gmail.com>
2014-02-19 00:29:25 -0500

    Redirect admin logout to CAS logout if enabled.

--- terpbooks/terpbooks/urls.py
+++ terpbooks/terpbooks/urls.py
@@ -1,11 +1,22 @@
 from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse_lazy
 from django.conf import settings
+from django.views.generic import RedirectView
 
 from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('')
+
+if settings.USE_CAS:
+    urlpatterns += patterns('',
+        url(r'^accounts/login$', 'cas.views.login', name='login'),
+        url(r'^accounts/logout$', 'cas.views.logout', name='logout'),
+        url('^admin/logout/$', RedirectView.as_view(url=reverse_lazy('logout'))),
+    )
+
+urlpatterns += patterns('',
     # Examples:
     # url(r'^$', 'terpbooks.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -15,9 +26,3 @@ urlpatterns = patterns('',
     url(r'^transactions/', include('transactions.urls')),
     url(r'^messages/', include('messages.urls')),
 )
-
-if settings.USE_CAS:
-    urlpatterns += patterns('',
-        url(r'^accounts/login$', 'cas.views.login', name='login'),
-        url(r'^accounts/logout$', 'cas.views.logout', name='logout'),
-    )
