08c44650a8c2d849b871bbe6dbaa054bd90431e3
Jacky Tian <xjtian94@gmail.com>
2014-04-04 11:38:41 -0400

    AuthorFormSet post view doesn't require already-entered data to be valid - using form[field].value() instead of cleaned_data

--- terpbooks/books/views.py
+++ terpbooks/books/views.py
@@ -24,17 +24,12 @@ class AuthorFormSetView(generic.View):
 
     def post(self, request, *args, **kwargs):
         posted_formset = AuthorFormSet(request.POST)
-        if not posted_formset.is_valid():
-            return render(request, 'sell/author_formset.html', {
-                'author_formset': posted_formset,
-                'error_message': 'Author fields must have at least a first and last name separated by a space.',
-            })
 
         initial = []
         for form in posted_formset:
             data = {}
             for field in form.fields:
-                data[field] = form.cleaned_data[field]
+                data[field] = form[field].value()
             initial.append(data)
 
         formset = AuthorFormSet(initial=initial)
