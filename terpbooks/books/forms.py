7ae63cdae680c86877c88c73ae8cbbd46e48c78e
Jacky Tian <xjtian94@gmail.com>
2014-04-03 21:29:46 -0400

    Refactor old ProfessorForm to base NameSplitForm. Implement AuthorForm using NameSplitForm.

--- terpbooks/books/forms.py
+++ terpbooks/books/forms.py
@@ -18,21 +18,28 @@ class SemesterForm(BootstrapModelForm):
         fields = ('semester', 'year', )
 
 
-class AuthorForm(BootstrapModelForm):
-    class Meta:
-        model = Author
-        fields = ('first_name', 'last_name', )
+class NameSplitBootstrapForm(BootstrapForm):
+    def __init__(self, model_class, *args, **kwargs):
+        self.first_name_field = kwargs.pop('first_name_field', 'first_name')
+        self.last_name_field = kwargs.pop('last_name_field', 'last_name')
+        self.model_class = model_class
 
+        self.name_field_name = kwargs.pop('name_field_name', 'name')
 
-class ProfessorForm(BootstrapForm):
-    professor = forms.CharField(max_length=60)
+        super(NameSplitBootstrapForm, self).__init__(*args, **kwargs)
+
+        self.fields[self.name_field_name] = forms.CharField(max_length=60)
+        self.fields[self.name_field_name].widget.attrs.update({'class': 'form-control'})
 
     def is_valid(self):
-        valid = super(ProfessorForm, self).is_valid()
+        valid = super(NameSplitBootstrapForm, self).is_valid()
         if not valid:
             return False
 
-        first_name, last_name = self.cleaned_data['professor'].split()
+        if len(self.cleaned_data['name_field_name']) == 0:
+            return True
+
+        first_name, last_name = self.cleaned_data[self.name_field_name].split()
         if len(first_name) == 0:
             self._errors['empty_fn'] = 'Empty first name'
             return False
@@ -43,12 +50,21 @@ class ProfessorForm(BootstrapForm):
         return True
 
     def save(self, commit=True):
-        fn, ln = self.cleaned_data['professor'].split()
-        if Professor.objects.filter(first_name=fn, last_name=ln).exists():
-            return Professor.objects.get(first_name=fn, last_name=ln)
+        if len(self.cleaned_data['name_field_name']) == 0:
+            return None
 
-        p = Professor(first_name=fn, last_name=ln)
+        fn, ln = self.cleaned_data['professor'].split()
+        if self.model_class.objects.filter(first_name=fn, last_name=ln).exists():
+            return self.model_class.objects.get(first_name=fn, last_name=ln)
+
+        p = self.model_class(**{self.first_name_field: fn, self.last_name_field: ln})
         if commit:
             p.save()
 
         return p
+
+
+class AuthorForm(NameSplitBootstrapForm):
+    def __init__(self, *args, **kwargs):
+        kwargs.update({'name_field_name': 'author'})
+        super(AuthorForm, self).__init__(Author, *args, **kwargs)
