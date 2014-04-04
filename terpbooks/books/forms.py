4ca3f1bbe86142415867f7c2807a9414fa58f5b6
Jacky Tian <xjtian94@gmail.com>
2014-04-04 13:50:58 -0400

    Refactor NameSplitBootstrapForm to use RegexValidator instead of custom is_valid()

--- terpbooks/books/forms.py
+++ terpbooks/books/forms.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from django import forms
 from django.forms.util import ErrorList
 from django.forms.formsets import formset_factory
+from django.core.validators import RegexValidator
 
 from terpbooks.forms import BootstrapForm, BootstrapModelForm
 from .models import Textbook, Semester, Professor, Author
@@ -64,44 +65,25 @@ class NameSplitBootstrapForm(forms.Form):
 
         super(NameSplitBootstrapForm, self).__init__(*args, **kwargs)
 
-        self.fields[self.name_field_name] = forms.CharField(max_length=60, required=False)
+        self.fields[self.name_field_name] = forms.CharField(max_length=60,
+                                                            required=False,
+                                                            validators=[RegexValidator(
+                                                                regex=r'^[^ ]+ [^ ]+( [^ ]+)*$',
+                                                                message='Names must have at 2 separate words. This '
+                                                                        'field can be left blank.'
+                                                            )])
         self.fields[self.name_field_name].widget.attrs.update({'class': 'form-control'})
-
-    def is_valid(self):
-        valid = super(NameSplitBootstrapForm, self).is_valid()
-        if not valid:
-            return False
-
-        if self.name_field_name not in self.cleaned_data or len(self.cleaned_data[self.name_field_name]) == 0:
-            return True
-
-        split = self.cleaned_data[self.name_field_name].split()
-        if len(split) < 2:
-            errors = self._errors.setdefault(self.name_field_name, ErrorList())
-            errors.append(u'Names must have at 2 separate words. This field can be left blank.')
-            return False
-
-        if len(split) > 2:
-            split[1] = u' '.join(split[1:])
-            split = split[:2]
-
-        first_name, last_name = split
-        if len(first_name) == 0:
-            errors = self._errors.setdefault(self.name_field_name, ErrorList())
-            errors.append(u'Empty first name')
-            return False
-        if len(last_name) == 0:
-            errors = self._errors.setdefault(self.name_field_name, ErrorList())
-            errors.append(u'Empty last name')
-            return False
-
-        return True
 
     def save(self, commit=True):
         if self.name_field_name not in self.cleaned_data or len(self.cleaned_data[self.name_field_name]) == 0:
             return None
 
-        fn, ln = self.cleaned_data[self.name_field_name].split()
+        split = self.cleaned_data[self.name_field_name].split()
+        if len(split) > 2:
+            split[1] = u' '.join(split[1:])
+            split = split[:2]
+
+        fn, ln = split
         if self.model_class.objects.filter(**{
             self.first_name_field: fn, self.last_name_field: ln
         }).exists():
